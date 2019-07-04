979d9bc2a1da4f0758965bd7f6869a7fc68dbd7e
FUTOSHI <36586840+futo0713@users.noreply.github.com>
2019-07-03 21:53:02 -0400

    Add files via upload

--- tensorflow/0_tensorflow_main_test.py
+++ tensorflow/0_tensorflow_main_test.py
@@ -11,9 +11,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #==========================================================
 import pickle
 
-print('- What kind of data?')
-
-name = input()
+name = 'sin'
 dataset_dir = 'C:\\Users\FutoshiTsutsumi\Desktop\python_test\\tensorflow\dataset_sample\pickle'
 save_file = dataset_dir + '\dataset_{}.pkl'.format(name)
 
@@ -28,11 +26,15 @@ import tensorflow as tf
 holder_x = tf.placeholder(tf.float32)
 holder_t = tf.placeholder(tf.float32)
 
-w = tf.Variable(tf.random_normal([1],mean=0.0, stddev=1.0))
-b = tf.Variable(tf.random_normal([1],mean=0.0, stddev=1.0))
-y = holder_x * w + b
+Wh = tf.Variable(tf.random_normal([20,1],mean=0.0, stddev=1.0))
+Bh = tf.Variable(tf.random_normal([20,1],mean=0.0, stddev=1.0))
+H = tf.sigmoid(holder_x * Wh + Bh)
 
-loss = tf.reduce_mean(tf.square(y - holder_t))
+Wo = tf.Variable(tf.random_normal([1,20],mean=0.0, stddev=1.0))
+Bo = tf.Variable(tf.random_normal([1],mean=0.0, stddev=1.0))
+Y = tf.matmul(Wo, H) + Bo
+
+loss = tf.reduce_mean(tf.square(Y - holder_t))
 
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
@@ -40,22 +42,33 @@ train = optimizer.minimize(loss)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(1001):
-        if step % 100 == 0:
+    for step in range(5001):
+        if step % 300 == 0:
             print((step, sess.run(loss, feed_dict={holder_x: x,holder_t: t})))
         sess.run(train, feed_dict={holder_x: x,holder_t: t})
 
-    weight = sess.run(w)
-    bias = sess.run(b)
+    weight_h = sess.run(Wh)
+    bias_h = sess.run(Bh)
+    weight_o = sess.run(Wo)
+    bias_o = sess.run(Bo)
+
 
 #Time required
 e = time.time()
 print('time:' + str(e-s))
 
 #Plot graph
+import numpy as np
 import matplotlib.pyplot as plt
 
 plt.plot(x,t,'.')
-plt.plot(x,x*weight+bias)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+H = sigmoid(x*weight_h+bias_h)
+Y = np.dot(weight_o,H) + bias_o
+
+plt.plot(x,Y.reshape(-1),'.')
 plt.ylim(0)
 plt.show()
